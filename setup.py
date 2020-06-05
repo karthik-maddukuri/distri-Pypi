1b77becaf0490ea0aa74ecd56d052cc7448f9f96
Karthik Maddukuri <kmaddukuri6@gmail.com>
2020-06-05 17:34:50 -0500

    add files - pep8

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 from setuptools import setup
 
-setup(name='distributions',
-      version='0.1',
-      description='Gaussian and Binomial distributions',
-      packages=['distributions'],
-      zip_safe=False)
+setup(name='distributions', version='0.1', description='Gaussian and Binomial distributions',
+      packages=['distributions'], zip_safe=False)
